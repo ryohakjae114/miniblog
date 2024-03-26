1d9233d52aef5e66781ad7f3f8a4c903eec7c901
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-03-26 14:19:23 +0900

    postを画面から作成できるように

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to new_post_path, notice: '投稿しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+end
