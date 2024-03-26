19509cb4d2afba155cabb329610aff1489fffbac
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-03-26 16:40:22 +0900

    routesにindexアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  resources :posts, only: %i[new create]
+  resources :posts, only: %i[index new create]
 end
