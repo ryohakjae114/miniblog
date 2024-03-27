8bf85fa05b6e756b4aec313b7c9df040b30b8788
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-03-27 18:10:06 +0900

    エラーが起きないようにuserのemailに対するvalidatesをオフにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
                    format: { with: /\A[a-zA-Z]+\z/, message: '英文字のみが使えます。スペースも禁止です。' }
   validates :profile, length: { maximum: 200 }
   validates :external_blog_url, length: { maximum: 2083 }
+
+  def email_required?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end
