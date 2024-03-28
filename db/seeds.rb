c63752738a7895c58ddc84a582476d898d4179bf
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-03-28 14:42:55 +0900

    ユーザを作成する、ユーザに紐づいたサンプルデータを作成するよう、seeds.rbを編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+user = User.create(name: 'hakjae', password: '12341234', password_confirmation: '12341234', profile: 'りょはっちぇです',
+                   external_blog_url: 'hakjae_blog@example.com')
 10.times do
-  Post.create(body: Faker::Lorem.paragraph_by_chars(number: rand(10..140)))
+  user.posts.create(body: Faker::Lorem.paragraph_by_chars(number: rand(10..140)))
 end
