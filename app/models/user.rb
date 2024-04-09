fe62cc863784ebfba3a589a9550cd1e94d7fa8b0
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-04-09 11:49:12 +0900

    パスワード変更機能を削除する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
 
