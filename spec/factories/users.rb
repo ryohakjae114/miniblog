ad9e6e8fbcc6e8a76ba4d077516bfe2da9ff559b
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-04-15 16:37:03 +0900

    テストのリファクタリング

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:name) { |n| "hakjae#{[*'a'..'z'][n]}" }
+    sequence(:name) { |n| "hakjae#{[*'a'..'z'][n - 1]}" }
     password { '1234512345' }
     password_confirmation { '1234512345' }
   end
