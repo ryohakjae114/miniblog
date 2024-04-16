6d4291a43cf8848db2cc0175362466363b25cf90
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-04-16 17:28:03 +0900

    ユーザプロフィール情報画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'posts#index'
-  resources :posts, only: %i[new create show]
+  resources :users, only: :show
+  resources :posts, only: %i[new create]
 end
