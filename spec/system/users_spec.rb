4f5227b17e779414719d86e50cca5787f2c47035
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-04-17 11:11:51 +0900

    ユーザプロフィール情報画面のテスト作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe 'Users', type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+
+  it 'ユーザ詳細ページにアクセスできること' do
+    sign_in user
+    visit user_path(user)
+    expect(page).to have_content('ユーザプロフィール情報')
+  end
 end
