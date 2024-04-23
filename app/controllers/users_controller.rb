97817015f274c1e91f75a42cc72ea945b5163017
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-04-23 16:45:31 +0900

    ログインしているユーザは自身の公開情報のみ変更できるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show profile update]
+  before_action :authenticate_current_user!, only: :update
 
   def show
   end
@@ -9,7 +10,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to edit_public_user_url(@user), notice: t('users.edit_public.update_success')
+      redirect_to profile_user_path(@user), notice: t('users.edit_public.update_success')
     else
       render :edit_public, status: :unprocessable_entity
     end
@@ -23,5 +24,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:introduction, :external_blog_url)
+  end
+
+  def authenticate_current_user!
+    unless current_user == @user
+      redirect_to root_url
+    end
   end
 end
