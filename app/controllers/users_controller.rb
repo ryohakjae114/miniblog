e907a985f69063bcd03724a45c05fb084ff9e2b5
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-04-30 14:05:42 +0900

    不要なアクションを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show update]
-  before_action :authenticate_current_user!, only: :update
+  before_action :set_user, only: :show
 
   def show
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to user_path(@user), notice: t('users.edit_public.update_success')
-    else
-      render :profile, status: :unprocessable_entity
-    end
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
-  end
-
-  def user_params
-    params.require(:user).permit(:introduction, :external_blog_url)
-  end
-
-  def authenticate_current_user!
-    unless current_user == @user
-      redirect_to root_url
-    end
   end
 end
