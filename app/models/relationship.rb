2edc548641225ebe0ff04b9208ee39c1869187ff
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-01 15:27:12 +0900

    ユーザフォロー機能の多対多の実装を完成

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,4 @@
 class Relationship < ApplicationRecord
+  belongs_to :follower, class_name: 'User', inverse_of: :active_relationships
+  belongs_to :followed, class_name: 'User', inverse_of: :passive_relationships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
 
   has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: 'follower_id',
-                                  dependent: :destroy
+                                  dependent: :destroy,
+                                  inverse_of: :follower
   has_many :following, through: :active_relationships, source: :followed
   has_many :passive_relationships, class_name: 'Relationship',
                                    foreign_key: 'followed_id',
-                                   dependent: :destroy
+                                   dependent: :destroy,
+                                   inverse_of: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
   has_many :posts, dependent: :destroy
