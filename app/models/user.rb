09cde2583d416c81acfd1e4cc00c144f114a03ce
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-01 13:38:30 +0900

    フォロー機能を実装するためのモデル作成(relationship.rbの設定はまだ)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 
+  has_many :active_relationships, class_name: 'Relationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name: 'Relationship',
+                                   foreign_key: 'followed_id',
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   has_many :posts, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 20 },
