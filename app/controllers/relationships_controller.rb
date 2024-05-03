1be01d12cd1aa5218a579ed328ac59e82cb9fd1d
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-03 13:56:16 +0900

    フォロー、アンフォローしたあとは元のページに戻る

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
-    redirect_to root_url
+    redirect_back_or_to root_url
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(@user)
-    redirect_to root_url, status: :see_other
+    redirect_back_or_to root_url, status: :see_other
   end
 end
