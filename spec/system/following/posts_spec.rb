60ef0d18a6568fbe0ce15ed703f470b6d27990a2
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-07 10:58:46 +0900

    フォロー、アンフォロー機能のテストの品質を上げる

--- spec/system/following/posts_spec.rb
+++ spec/system/following/posts_spec.rb
@@ -16,9 +16,12 @@ RSpec.describe 'Following::Posts', type: :system do
     user.follow(taji)
     visit following_posts_path
     expect(page).to have_css('.card', count: 2)
+    expect(page).to have_content('hakjae')
+    expect(page).to have_content('taji')
     user.unfollow(taji)
     visit current_path
     expect(page).to have_css('.card', count: 1)
     expect(page).to have_content('hakjae')
+    expect(page).not_to have_content('taji')
   end
 end
