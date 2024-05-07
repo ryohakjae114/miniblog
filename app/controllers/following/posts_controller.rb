7313bfda48052218d9cc0df3e476b38af23d1195
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-07 11:12:04 +0900

    postコントローラで効率よくdb検索できるように修正

--- app/controllers/following/posts_controller.rb
+++ app/controllers/following/posts_controller.rb
@@ -1,5 +1,5 @@
 class Following::PostsController < ApplicationController
   def index
-    @posts = Post.where(user: current_user.following)
+    @posts = Post.where(user: current_user.following_ids)
   end
 end
