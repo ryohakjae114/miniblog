6bad28310e23b2505e98b82fd79e4a0563d6978f
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-09 15:14:33 +0900

    いいねを解除できること、投稿をいいねした人を一覧できることのテストを追加

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Likes', type: :system do
-  let!(:user) { create(:user) }
+  let!(:user) { create(:user, name: 'hakjae') }
   let!(:post) { create(:post) }
 
   before do
@@ -15,5 +15,22 @@ RSpec.describe 'Likes', type: :system do
       find('.not-like').click
     end.to change(post.liked_users.where(id: user.id), :count).by(1)
     find_link('1件')
+  end
+
+  it 'いいねを解除できること' do
+    create(:like, user:, post:)
+    visit root_path
+    find_link('1件')
+    expect do
+      find('.like').click
+    end.to change(post.liked_users.where(id: user.id), :count).by(-1)
+    find_link('0件')
+  end
+
+  it '投稿をいいねした人を一覧できること' do
+    create(:like, user:, post:)
+    visit root_path
+    click_on '1件'
+    expect(page).to have_content('hakjae')
   end
 end
