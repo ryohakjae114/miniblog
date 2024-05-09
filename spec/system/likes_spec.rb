ae150b3273aab60c67cacc4080df72d18596bbcb
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-09 17:21:08 +0900

    いいね機能のテストリファクタ

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Likes', type: :system do
-  let!(:user) { create(:user, name: 'hakjae') }
+  let!(:hakjae) { create(:user, name: 'hakjae') }
+  let!(:taji) { create(:user, name: 'taji') }
   let!(:post) { create(:post) }
 
   before do
-    sign_in user
+    sign_in hakjae
   end
 
   it 'いいねできること' do
@@ -13,24 +14,27 @@ RSpec.describe 'Likes', type: :system do
     find_link('0件')
     expect do
       find('.not-like').click
-    end.to change(post.liked_users.where(id: user.id), :count).by(1)
+    end.to change(hakjae.likes, :count).by(1)
     find_link('1件')
   end
 
   it 'いいねを解除できること' do
-    create(:like, user:, post:)
+    create(:like, user: hakjae, post:)
     visit root_path
     find_link('1件')
     expect do
       find('.like').click
-    end.to change(post.liked_users.where(id: user.id), :count).by(-1)
+    end.to change(hakjae.likes, :count).by(-1)
     find_link('0件')
   end
 
   it '投稿をいいねした人を一覧できること' do
-    create(:like, user:, post:)
+    create(:like, user: hakjae, post:)
+    create(:like, user: taji, post:)
     visit root_path
-    click_on '1件'
-    expect(page).to have_content('hakjae')
+    click_on '2件'
+    expect(page).to have_content('いいねしたユーザ一覧(2人)')
+    expect(page).to have_link('hakjae')
+    expect(page).to have_link('taji')
   end
 end
