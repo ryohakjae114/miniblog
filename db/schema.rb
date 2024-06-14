a8bd5fb29d6a66a6da617b9c61b3ccb38a6cb095
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-06-14 17:23:34 +0900

    postモデルにcommentをカウントするcounter_cacheを設定する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_074628) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_14_075957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_14_074628) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.integer "likes_count", default: 0, null: false
+    t.integer "comments_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
