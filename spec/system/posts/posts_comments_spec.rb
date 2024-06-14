5f6743e18431786a2cf8dd18d142b5dfcb9396fe
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-06-14 16:27:17 +0900

    投稿に対するコメント機能のテストに、ざっくりやることを書いた

--- spec/system/posts/posts_comments_spec.rb
+++ spec/system/posts/posts_comments_spec.rb
@@ -13,14 +13,20 @@ RSpec.describe 'Posts::Comments', type: :system do
     within('.comment-count') do
       click_on '2件'
     end
+    # 投稿に対するコメントの総数を表示する
+    # 投稿に紐づいたcommentモデル作成
+    # 投稿のコメント総数を格納するcounter_cacheが良さそう
     expect(page).to have_content('コメント一覧')
+    # 　 コメント一覧画面を作成
     expect(page).to have_content('.comment'), count: 2
     expect(page).to have_content('2つ目のコメント')
+    # コメント一覧を投稿作成が新しい順に表示する
     expect(page).to have_content('1つ目のコメント')
   end
 
   it '投稿にコメントができること' do
     visit post_comments_path(post)
+    # コメント作成画面を作成する
     click_on 'コメントする'
     fill_in '投稿文',	with: 'Wryyyyyyy'
     click_on 'コメントする'
