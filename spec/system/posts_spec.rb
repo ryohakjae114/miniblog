14f036a03d4fa9984bfcd8f6439d3193f1867b3a
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-06-20 11:01:56 +0900

    コメント投稿機能のタスクを分けたので、そのテストを削除

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -32,5 +32,21 @@ RSpec.describe 'Posts', type: :system do
       expect(page).to have_content('あいうえお')
       expect(page.all('.post').count).to eq 2
     end
+
+    it 'コメント一覧できること' do
+      visit root_path
+      within('.comment-count') do
+        click_on '2件'
+      end
+      # 投稿に対するコメントの総数を表示する
+      # 投稿に紐づいたcommentモデル作成
+      # 投稿のコメント総数を格納するcounter_cacheが良さそう
+      expect(page).to have_content('コメント一覧')
+      # 　 コメント一覧画面を作成
+      expect(page).to have_content('.comment'), count: 2
+      expect(page).to have_content('2つ目のコメント')
+      # コメント一覧を投稿作成が新しい順に表示する
+      expect(page).to have_content('1つ目のコメント')
+    end
   end
 end
