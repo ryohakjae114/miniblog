cc95162fe7707fb3ea8e33fcd17ba2b7763f2177
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-06-20 16:04:41 +0900

    コメント作成画面を作成

---
+++ app/controllers/posts/comments_controller.rb
@@ -0,0 +1,27 @@
+class Posts::CommentsController < ApplicationController
+  before_action :set_post
+
+  def new
+    @comment = @post.comments.build(user: current_user)
+  end
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to post_path(@post), notice: t('controller.created')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
