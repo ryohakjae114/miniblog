f93c63001de26012bbe000d4922a7102378aeeb3
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-06-20 16:46:16 +0900

    コメント新規作成機能を作成

--- spec/system/posts/posts_comments_spec.rb
+++ spec/system/posts/posts_comments_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts::Comments', type: :system do
+  let!(:user) { create(:user) }
+  let!(:post) { create(:post, body: '大局着眼 小局着手', user:) }
+
+  before do
+    sign_in user
+  end
+
   it '投稿にコメントができること' do
     visit post_path(post)
-    click_on 'コメントする'
+    click_on 'コメント作成'
     expect(page).to have_content('コメント作成')
-    fill_in '投稿文',	with: 'Wryyyyyyy'
-    click_on 'コメント投稿'
-    expect(page).to have_content('コメントしました')
-    expect(all('.comment')[1].text).to eq('Wryyyyyyy')
+    fill_in 'コメント文',	with: 'Wryyyyyyy'
+    click_on '登録する'
+    expect(page).to have_content('新規登録しました')
+    expect(page).to have_selector('.comment-body', match: :first), text: 'Wryyyyyyy'
   end
 end
