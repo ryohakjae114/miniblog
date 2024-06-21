ef8e7971263cf7163cdd8c0784b5477d020e72fd
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-06-21 09:42:26 +0900

    テストのコメントを削除

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -40,14 +40,9 @@ RSpec.describe 'Posts', type: :system do
       within('.post') do
         click_on '大局着眼 小局着手'
       end
-      # 投稿に対するコメントの総数を表示する
-      # 投稿に紐づいたcommentモデル作成
-      # 投稿のコメント総数を格納するcounter_cacheが良さそう
       expect(page).to have_content('投稿詳細')
-      # 　 コメント一覧画面を作成
       expect(page).to have_css('.comment'), count: 2
       expect(page).to have_content('役に立った')
-      # コメント一覧を投稿作成が昇順に表示する
       expect(page).to have_content('役に立たなかった')
     end
   end
