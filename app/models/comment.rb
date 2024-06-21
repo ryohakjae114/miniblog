4db6c13bbd2f74ad1cbaf55c79c6ebc86764e2f2
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-06-21 14:58:34 +0900

    コメントに対するコメントモデルを作成

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: true
+  has_many :child_comments, class_name: 'Comment', foreign_key: 'parent_comment_id', inverse_of: :parent_comment, dependent: :destroy
+  belongs_to :parent_comment, class_name: 'Comment', inverse_of: :child_comments, optional: true
 
   validates :body, presence: true, length: { maximum: 140 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
-user = User.create(name: 'hakjae', password: '1234512345', password_confirmation: '1234512345', introduction: 'りょはっちぇです',
-                   external_blog_url: 'https://hakjae@example.com')
-10.times do
-  post = user.posts.create!(body: Faker::Lorem.paragraph_by_chars(number: rand(10..140)))
+taji = User.create(name: 'taji', password: '1234512345', password_confirmation: '1234512345', introduction: 'tajiです',
+                   external_blog_url: 'https://taji@example.com')
+hakjae = User.create(name: 'hakjae', password: '1234512345', password_confirmation: '1234512345', introduction: 'りょはっちぇです',
+                     external_blog_url: 'https://hakjae@example.com')
+5.times do
+  post = hakjae.posts.create!(body: Faker::Lorem.paragraph_by_chars(number: rand(10..140)))
   2.times do
-    user.comments.create!(post:, body: Faker::Lorem.paragraph_by_chars(number: rand(10..140)))
+    comment = taji.comments.create!(post:, body: Faker::Lorem.paragraph_by_chars(number: rand(10..140)))
+    comment.child_comments.create!(body: Faker::Lorem.paragraph_by_chars(number: rand(10..140)), user: hakjae, post_id: comment.post_id)
   end
 end
