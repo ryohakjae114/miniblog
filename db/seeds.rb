4b60cd22738316d6f864d06a0ab25076ea5a6edb
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-06-26 14:02:55 +0900

    三つ目の階層には、コメント作成ボタンを置かない

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-taji = User.create(name: 'taji', password: '1234512345', password_confirmation: '1234512345', introduction: 'tajiです',
-                   external_blog_url: 'https://taji@example.com')
-hakjae = User.create(name: 'hakjae', password: '1234512345', password_confirmation: '1234512345', introduction: 'りょはっちぇです',
-                     external_blog_url: 'https://hakjae@example.com')
+taji = User.create!(name: 'taji', password: '1234512345', password_confirmation: '1234512345', introduction: 'tajiです',
+                    external_blog_url: 'https://taji@example.com')
+hakjae = User.create!(name: 'hakjae', password: '1234512345', password_confirmation: '1234512345', introduction: 'りょはっちぇです',
+                      external_blog_url: 'https://hakjae@example.com')
 5.times do
   post = hakjae.posts.create!(body: Faker::Lorem.paragraph_by_chars(number: rand(10..140)))
   2.times do
