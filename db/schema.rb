fa6e26d9d03a15099055517f82a0e4ab4b2612e3
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-07-08 12:19:38 +0900

    userにemailカラムを追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_25_090158) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_08_025427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_25_090158) do
     t.text "external_blog_url", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
