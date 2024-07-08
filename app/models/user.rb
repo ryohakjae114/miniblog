ff7fb454f32677629125636c2b5f27733a948d00
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-07-08 11:59:05 +0900

    userにemailカラムを追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 20 }, uniqueness: true,
                    format: { with: /\A[a-zA-Z]+\z/, message: I18n.t('activerecord.validates.user.name.format') }
+  validates :email, length: { length: { maximum: 254 } }
   validates :introduction, length: { maximum: 200 }
   validates :external_blog_url, length: { maximum: 2083 }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_20_054634) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_090158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string "body", limit: 140, default: "", null: false
     t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parent_comment_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id", "post_id"], name: "index_comments_on_user_id_and_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
@@ -69,6 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_20_054634) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "comments", column: "parent_comment_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
