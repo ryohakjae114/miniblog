281680f652430af2c5c15b7113457326fd929853
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-07-08 13:32:39 +0900

    /letter_openerでメールを確認できる

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   devise_for :users
   root 'posts#index'
   resources :users, only: :show
