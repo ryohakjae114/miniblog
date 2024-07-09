eb4bc34b6db714e01792e7590fa7fad6fe2047e0
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-07-09 14:18:35 +0900

    コメント作成後にメールが飛ぶことのテスト作成

--- spec/system/posts/posts_comments_spec.rb
+++ spec/system/posts/posts_comments_spec.rb
@@ -1,20 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts::Comments', type: :system do
-  let!(:user) { create(:user) }
-  let!(:post) { create(:post, body: '大局着眼 小局着手', user:) }
+  let!(:hakjae) { create(:user, name: 'hakjae') }
+  let!(:taji) { create(:user, name: 'taji', email: 'taji@example.com') }
+  let!(:post) { create(:post, body: '大局着眼 小局着手', user: taji) }
 
   before do
-    sign_in user
+    sign_in hakjae
   end
 
-  it '投稿にコメントができること' do
+  it '投稿にコメントができ、投稿主にメールを送信できること' do
     visit post_path(post)
+    expect(ActionMailer::Base.deliveries.count).to eq 0
     click_on 'コメント作成'
     expect(page).to have_current_path new_post_comment_path(post)
     fill_in 'コメント',	with: 'Wryyyyyyy'
     click_on '登録する'
     expect(page).to have_content('新規登録しました')
     expect(page).to have_selector('.comment-body', match: :first), text: 'Wryyyyyyy'
+    expect(ActionMailer::Base.deliveries.count).to eq 1
+    mail = ActionMailer::Base.deliveries.last
+    expect(mail.to.first).to eq 'taji@example.com'
+    expect(mail.subject).to eq 'コメントが届きました'
+    expect(mail.html_part.body).to have_link 'コメントを見る', href: "http://miniblog.test:3000#{post_path(post, anchor: Comment.last)}"
   end
 end
