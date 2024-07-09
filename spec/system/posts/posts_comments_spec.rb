160e018701f0c5d2ac58b142c5fc51baef36c133
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-07-09 14:27:23 +0900

    postしたユーザ本人のコメントは、通知が飛ばないことのテスト作成

--- spec/system/posts/posts_comments_spec.rb
+++ spec/system/posts/posts_comments_spec.rb
@@ -3,17 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Posts::Comments', type: :system do
   let!(:hakjae) { create(:user, name: 'hakjae') }
   let!(:taji) { create(:user, name: 'taji', email: 'taji@example.com') }
-  let!(:post) { create(:post, body: '大局着眼 小局着手', user: taji) }
+  let!(:taji_post) { create(:post, body: '大局着眼 小局着手', user: taji) }
+  let!(:hakjae_post) { create(:post, user: hakjae) }
 
   before do
     sign_in hakjae
   end
 
   it '投稿にコメントができ、投稿主にメールを送信できること' do
-    visit post_path(post)
+    visit post_path(taji_post)
     expect(ActionMailer::Base.deliveries.count).to eq 0
     click_on 'コメント作成'
-    expect(page).to have_current_path new_post_comment_path(post)
+    expect(page).to have_current_path new_post_comment_path(taji_post)
     fill_in 'コメント',	with: 'Wryyyyyyy'
     click_on '登録する'
     expect(page).to have_content('新規登録しました')
@@ -22,6 +23,12 @@ RSpec.describe 'Posts::Comments', type: :system do
     mail = ActionMailer::Base.deliveries.last
     expect(mail.to.first).to eq 'taji@example.com'
     expect(mail.subject).to eq 'コメントが届きました'
-    expect(mail.html_part.body).to have_link 'コメントを見る', href: "http://miniblog.test:3000#{post_path(post, anchor: Comment.last)}"
+    expect(mail.html_part.body).to have_link 'コメントを見る', href: "http://miniblog.test:3000#{post_path(taji_post, anchor: Comment.last)}"
+  end
+
+  it '自身の投稿にコメントした場合、メールは送信されないこと' do
+    expect(ActionMailer::Base.deliveries.count).to eq 0
+    create(:comment, post: hakjae_post, user: hakjae)
+    expect(ActionMailer::Base.deliveries.count).to eq 0
   end
 end
