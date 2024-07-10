8c1876039911a500daadc5a5e8ba5475672bba1d
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-07-10 13:58:43 +0900

    一覧画面でもし画像があれば、画像を表示する

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   has_many :comments, dependent: :destroy
-  has_one_attached :picture
+  has_one_attached :picture do |attachable|
+    attachable.variant :thumb, resize_to_limit: [300, 300]
+  end
 
   validates :body, presence: true, length: { maximum: 140 }
 end
