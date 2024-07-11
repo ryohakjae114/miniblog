5413ce4219db1eb42bc9412d79a60a076032d31a
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-07-11 11:31:42 +0900

    投稿詳細画面では大きいサイズの写真を表示する

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one_attached :picture do |attachable|
     attachable.variant :thumb, resize_to_limit: [300, 300]
+    attachable.variant :eyecatch, resize_to_limit: [500, 500]
   end
 
   validates :body, presence: true, length: { maximum: 140 }
