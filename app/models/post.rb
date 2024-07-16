04c2f9353096062187db712037f2941a45f0e6aa
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-07-16 13:53:32 +0900

    前日のいいねが多かった記事トップ10を取得する

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,12 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { maximum: 140 }
   validate :allowed_picture_content_type
 
+  def self.yesterday_liked_ranking_top_ten
+    yesterday_range = DateTime.new(2024, 7, 16, 0, 0, 0)..DateTime.new(2024, 7, 16, 23, 59, 59)
+    select('posts.*',
+           'count(likes.id) AS likes').left_joins(:likes).where(likes: { created_at: yesterday_range }).group('posts.id').order('likes DESC').limit(10)
+  end
+
   def allowed_picture_content_type
     if picture.attached? && !picture.content_type.in?(%('image/heic image/jpeg image/png'))
       errors.add(:picture, I18n.t('activerecord.validates.user.picture.allowed_picture_content_type'))
