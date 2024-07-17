818bb56694ffc67c6f63fe7cd16672e334298194
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-07-17 15:49:28 +0900

    メールが届かない問題を解決

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -3,6 +3,8 @@ class PostMailer < ApplicationMailer
 
   def notice_liked_ranking
     @posts = Post.yesterday_liked_ranking_top_ten
-    mail(to: User.pluck(:email).delete(''), subject: default_i18n_subject)
+    emails = User.pluck(:email)
+    emails.delete('')
+    mail(to: emails, subject: default_i18n_subject)
   end
 end
