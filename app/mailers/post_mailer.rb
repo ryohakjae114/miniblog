f2257bd4970fff9ca41c88d1b92db2219fae34ec
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-07-17 17:38:48 +0900

    メールの送りもとを統一する

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,6 +1,4 @@
 class PostMailer < ApplicationMailer
-  default from: 'noreply@example.com'
-
   def notice_liked_ranking
     @posts = Post.yesterday_liked_ranking_top_ten
     emails = User.pluck(:email)
